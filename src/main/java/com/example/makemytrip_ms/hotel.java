b7973647fe24149af3d7162d222d34e3013950ad
Arif Shaikh <shaikharif006@gmail.com>
2024-10-04 01:09:58 +0530

    coderefactor

--- src/main/java/com/example/makemytrip_ms/hotel.java
+++ src/main/java/com/example/makemytrip_ms/hotel.java
@@ -4,8 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class flight {
-    @GetMapping("/flight")
-    public String getData() {return "Please book Air Indigo" ;}
+public class hotel {
+    @GetMapping("/hotel")
+    public String getData() {return "Taj India" ;}
 }
-
